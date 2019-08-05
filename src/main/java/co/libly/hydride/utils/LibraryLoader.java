2064ff8cb3492d8be45c5a4e1624bdafc7621642
Gurpreet Paul <gurpreet@gurpreet.co>
2019-08-05 19:22:33 +0100

    Add armv6

--- src/main/java/co/libly/hydride/utils/LibraryLoader.java
+++ src/main/java/co/libly/hydride/utils/LibraryLoader.java
@@ -300,8 +300,17 @@ public final class LibraryLoader {
     public void setPermissions(File file) throws IOException{
         Set<PosixFilePermission> perms = new HashSet<>();
         perms.add(PosixFilePermission.OWNER_READ);
+        perms.add(PosixFilePermission.OWNER_WRITE);
+        perms.add(PosixFilePermission.OWNER_EXECUTE);
+
         perms.add(PosixFilePermission.OTHERS_READ);
+        perms.add(PosixFilePermission.OTHERS_WRITE);
+        perms.add(PosixFilePermission.OTHERS_EXECUTE);
+
         perms.add(PosixFilePermission.GROUP_READ);
+        perms.add(PosixFilePermission.GROUP_WRITE);
+        perms.add(PosixFilePermission.GROUP_EXECUTE);
+
         Files.setPosixFilePermissions(file.toPath(), perms);
     }
 
